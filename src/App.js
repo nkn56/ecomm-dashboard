d0a71a3b1282f3bb283914eb06e7c17e15462468
nkn56 <ntshnpt@yahoo.com>
2021-06-16 18:24:45 +0530

    create simple app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+import {Button} from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+      <p>ECOMM Project</p>
+      <button>Normal Button</button>
+      <Button>Bootstrap Button</Button>
+    </div>
+  );
+}
+
+export default App;
